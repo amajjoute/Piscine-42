e0c04b1c20cb2d7c2d2be64bee38b58ad6b3f616
SALIM <amajjoute2@gmail.com>
2025-04-16 02:18:20 +0100

    Update ft_print_combn.c

--- C/c00/ex08/ft_print_combn.c
+++ C/c00/ex08/ft_print_combn.c
@@ -14,16 +14,13 @@
 
 void	print_combn(char	*num, int	num_size,	int	index,	char	digit)
 {
-	if (index == num_size)
-	{
+	if (index == num_size) {
 		write(1, num, num_size);
 		if (num[0] - '0' < 10 - num_size)
 			write(1, ", ", 2);
 	}
-	else
-	{
-		while (digit - '0' <= (10 - num_size) + index)
-		{
+	else {
+		while (digit - '0' <= (10 - num_size) + index) {
 			num[index] = digit;
 			print_combn(num, num_size, index + 1, digit + 1);
 			digit++;
@@ -31,15 +28,13 @@ void	print_combn(char	*num, int	num_size,	int	index,	char	digit)
 	}
 }
 
-void	ft_print_combn(int	n)
-{
+void	ft_print_combn(int n) {
 	char	num[9];
 
 	if (n > 0 && n < 10)
 		print_combn(num, n, 0, '0');
 }
 
-int	main(void)
-{
+int	main(void) {
 	ft_print_combn(5);
 }
